47c488ae33b3fbe0fc6c5326dc786b362c2e3aed
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-03 19:40:16 +0000

    This crashes :3
    crashmaxing

---
+++ Day#1/FILO_Testing.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define float EarthGravity = 9.8f;
+
+int main()
+{
+    int x,y = 0;
+    int yMax,xMax= 255;
+    int yLower,xLower= 255;
+
+    while(x != xMax)
+    {
+        printf("Your number is %d\n", x);
+        x++;
+
+    }
+
+
+
+    return 0;
+}
