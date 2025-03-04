9b8d4f4fef205bab28842db456fdc639b3b83769
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-04 00:19:19 +0000

    TempConversion
    temp conversion witha a funciton converter instead of fixed formila

--- day#2/TempConversion.c
+++ day#2/TempConversion.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <ctype.h>
 
+float FehConvert(float celc)
+{
+    float feh;
+    feh = celc * 9/5 +32;
+
+    return feh;
+}
+
 int main()
 {
     float c;
@@ -9,7 +17,7 @@ int main()
     printf("Input the Celcius and i will convert it to Fehreneit: ");
     scanf("%f", &c);
 
-    feh = c * 9/5 + 32;
+    feh = FehConvert(c);
     
     printf("\nCelcius: %f\n", c);
     printf("Fehrenheit: %f\n", feh);
