fef7cbf254c9cb2ad0a54aa00a8a8baf11607b75
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-06 20:13:33 +0000

    for loop moment

--- Day#5/Lewps.c
+++ Day#5/Lewps.c
@@ -4,10 +4,17 @@ int main()
 {
     int i = 0;
 
+
+    printf("\nUse of while loop\n");
     while (i <= 5)
     {
         printf("i is currently: %d\n",i);
         i++;
+    }
+    printf("\nUse of for loop\n");
+    for(int f = 0; f <=5; f++)
+    {
+        printf("i is currently: %d\n",f);
     }
     
 
