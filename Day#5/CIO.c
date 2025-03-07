df13b97714ea20deb162fc428cc568632911666f
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-07 22:14:39 +0000

    Some day 6 stuff mostly done in writing and notes
    Writted phsyically in a book some notes and did some charecter counting was usless but learned alot kinda stil studying

--- Day#5/CIO.c
+++ Day#5/CIO.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
+    int c;
 
+    c = getchar();
+
+    while(c != EOF) // while c != eof ( end of file )
+    {
+        putchar(c);
+        c = getchar();
+    }
 
     return 0;
 }
---
+++ Day#6/CharCounter.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    long nc;
+
+    nc = 0;
+
+    while(getchar() != EOF)
+    {
+        nc++;
+        printf("Amount of charecters in total: %1d\n", nc);
+    }
+
+    return 0;
+}
