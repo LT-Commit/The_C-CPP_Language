51b0cf20ec1c9ed776893c4e00f6a1bf2a59fdd3
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-08 15:19:27 -0100

    timer

---
+++ Day#7/timer.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+    int min;
+    int secs;
+    printf("Input mins: ");
+    scanf("%d", &min);
+
+    secs = min * 60;
+    
+    for(int i = 0; i <= secs; i++)
+    {
+        printf("Time left = %d\n", secs - i);
+        sleep(1);
+    }
+
+    return 0;
+}
