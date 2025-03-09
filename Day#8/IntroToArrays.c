454cecfd750280d9af6d04fc0ff92514d6912a51
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-09 22:15:30 -0100

    realyl didnt do much today will do it right now after this episode

--- Day#8/IntroToArrays.c
+++ Day#8/IntroToArrays.c
@@ -15,5 +15,16 @@ int main()
 
 
     printf("\n");
+
+
+
+
+    char Numbers[] = "12345678903413213";
+    
+    for(short i = 0; i <= sizeof(Numbers); i++)
+    {
+        printf("%c", Numbers[i]);
+    }
+    printf("\n");
     return 0;
 }
