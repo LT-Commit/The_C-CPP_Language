ae69844839c5cf98b50936c80b95686319e7a71b
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-09 23:33:22 +0000

    ANother number type of array

--- Day#8/IntroToArrays.c
+++ Day#8/IntroToArrays.c
@@ -29,6 +29,23 @@ int main()
 
     //I want to store them on a txt document so i will try do that or mess around more with arrays we will see
 
+    int TenNumbers[9];
+    TenNumbers[0] = 1;
+    TenNumbers[1] = 1;
+    TenNumbers[2] = 1;
+    TenNumbers[3] = 1;
+    TenNumbers[4] = 1;
+    TenNumbers[5] = 1;
+    TenNumbers[6] = 1;
+    TenNumbers[7] = 1;
+    TenNumbers[8] = 1;
+    TenNumbers[9] = 1;
+    
+    for(short i = 0; i <= 9; i++)
+    {
+        printf("%d", TenNumbers[i]);
+    }
+    printf("\n");
 
     return 0;
 }
