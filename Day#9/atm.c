0fd39a39861e78436594553c1ebb98d95125c2cd
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-10 22:46:16 +0000

    full thing?

--- Day#9/atm.c
+++ Day#9/atm.c
@@ -5,11 +5,26 @@ int Balance = 100;
 char UserOption;
 int withdraw;
 int Addition;
+int TempMoni;
 
 void Welcome()
 {
     printf("Welcome to the ATM\n");
     printf("Please select your option:\n1. Show bal\n2. Add money\n3. Withdraw\n4. Quit\n");
+}
+
+void Addmoney()
+{
+    printf("How much money do you want to add?\n");
+    scanf("%d", &TempMoni);
+    Balance = TempMoni + Balance;
+}
+
+void Withdrawmoney()
+{
+    printf("How much money do you want to withdraw?\n");
+    scanf("%d", &TempMoni);
+    Balance = TempMoni - Balance;
 }
 
 int main()
@@ -24,12 +39,19 @@ int main()
         case '1':
             printf("Your balance is: %d\n", Balance);
             break;
+        case '2':
+            
+            Addmoney();
+            break;
+        case '3':
+            Withdrawmoney();
+            break;
         
         default:
+            printf("\nThank you for using the ATM!");
             break;
         }
         
-        break;
     }
 
 
