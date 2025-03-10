d23c3fb971c0c235559caf051dfff8479bf9015e
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-10 19:56:10 +0000

    Just simple project

---
+++ Day#9/simpleProg.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    char chest[] = "64x Chests";
+
+    for(int i = 0; i <= sizeof(chest); i++)
+    {
+        printf("%c", chest[i]);
+    }
+    printf("\n");
+
+    
+
+
+
+
+    
+
+    return 0;
+}
