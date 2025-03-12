d9fa96ce38766dba1e8b5e8bd87523642de7278a
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-12 16:34:25 +0000

    Get money done
    moni for game

--- Day#11/blackjack.cpp
+++ Day#11/blackjack.cpp
@@ -7,28 +7,71 @@
 // variables
 
 bool MainLoop = true;
-int Balance = 100;
-int InputtedBal;
+int Balance = 0;
+int GameMoney;
+bool GameLoop = true;
 
 
 // functions
 void TableWelcome();
 void PlayerHand();
 void HouseHand();
+int GameBal();
 
 // classes
 
 
+
 int main()
 {
-    TableWelcome();
+    TableWelcome(); //  Welcome to the table 
 
     while (MainLoop)
     {
-        TableWelcome();
-        std::cin >> InputtedBal;
+        std::cout<< "Your balance is: " << Balance;
+        GameMoney = GameBal();
+
+        while (GameLoop)
+        {
+            /* code */
+        }
+        
+
+        MainLoop = false;
     }
     
 
     return 0;
+}
+
+void TableWelcome()
+{
+    std:: cout << "\nWelcome to the table this is my first game so dont judge to hard: \n";
+    std:: cout << "Today you gonna be playing blackjack! \n";
+    std:: cout << "If you dont know how to play then just get to 21 ez, ok enjoy\n";
+}
+
+int GameBal()
+{
+    int i;
+    bool validInput =false;
+    while (!validInput)
+    {
+        std::cout << "\nHow much money do you want to play with?: ";
+        std::cin >> i;
+        
+        if(i>Balance)
+        {
+            std::cout << "\nYou dont have enough money for that input .. brokie\n";
+        }
+        else
+        {
+            Balance = Balance-i;
+            return i;
+        }
+
+    }
+    
+    
+
 }
