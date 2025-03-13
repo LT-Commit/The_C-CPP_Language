2475dedfafce8fd9bc94abf4b92cbd998bbbd1df
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-13 17:13:29 +0000

    PASS BY REFERENCE
    passing by reference for faster run time and saving memory, This is obviouse and i should have done it in the first place but lemem list why i did this:
    - Doesnt coppy the variables and data
    - Easier to read and write for other programs

--- Day#12/CarRental.cpp
+++ Day#12/CarRental.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-// Just a simple car dealership with a fixed ammopunt of cars with an array
+// Just a simple car dealership with a fixed amount of cars with an array
 
 // I will make another project with a vector array soon after this so its expandable
 
@@ -25,16 +25,17 @@ struct Car
     }
 
 };
-
-void ShowCarDetails(Car car);
+// Future function list
+void ShowCarDetails(const Car& cars); // Passing by reference instead of copying like i was before, this is way better for conserving memory for runtime
+void ListCars(const Car& cars);
 
 int main()
 {
-    Car cars[3] = 
+    Car cars[3] = // Fixed array will use vector when i expand the project
    {
     Car("MX-5", "Mazda", 2006 , 90),
-    Car("Carname", "Modelname", 3 , 3.3),
-    Car("Carname", "Modelname", 3 , 3.3)
+    Car("Cougar Eliminator", "Mercury", 1970 , 95),
+    Car("RX-7", "Mazda", 1991 , 3.3)
    };
 
    
@@ -42,20 +43,21 @@ int main()
    for( int i = 0; i < (sizeof(cars) / sizeof(cars[0])); i++)
    {
         ShowCarDetails(cars[i]);
-        std::cout << "\n";
    }
 
    return 0;
 }
 
-void ShowCarDetails(Car cars)
+void ShowCarDetails(const Car& cars)
 {
    std::cout<< "Car details: \n"<< cars.CarName << std::endl;
    std::cout << cars.ModelName << std::endl;
    std::cout << cars.Ageofcar << std::endl;
    std::cout << cars.RentalPrice << "$ per hour"<< std::endl;
-   if(cars.IsRented == false ) 
-   {std::cout << "Available for rent!\n";} 
-   else{std::cout << "UNAVAIBALE\n";}
-   std::cout << "---------------------------------------------\n";
+   //if(cars.IsRented == false ) 
+   //{std::cout << "Available for rent!\n";} 
+   //else{std::cout << "UNAAVAILABLE\n";}
+   std::cout << "Availability: " << (cars.IsRented ? "UNAVAILABLE" : "Available!\n"); // This is way better and easier to read than above
+   std::cout << "---------------------------------------------\n"; // Seperator of cars
 }
+
