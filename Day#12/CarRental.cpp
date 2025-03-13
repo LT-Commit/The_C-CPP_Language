8e7295f4c2a42e4919fddf1f527756fd2d6b62be
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-13 13:01:35 +0000

    Fixed passing bu struct to function
    yay

--- Day#12/CarRental.cpp
+++ Day#12/CarRental.cpp
@@ -26,14 +26,33 @@ struct Car
 
 };
 
+void ShowCarDetails(Car car);
+
 int main()
 {
     Car cars[3] = 
    {
-    Car("Carname", "Modelname", 3 , 3.3),
+    Car("MX-5", "Mazda", 2006 , 90),
     Car("Carname", "Modelname", 3 , 3.3),
     Car("Carname", "Modelname", 3 , 3.3)
    };
 
-   std::cout<< cars[0].CarName << std::endl;
+   int num = 0;
+
+   ShowCarDetails(cars[0]);
+
+
+   
+   return 0;
+}
+
+void ShowCarDetails(Car cars)
+{
+   std::cout<< "Our first car is : \n"<< cars.CarName << std::endl;
+   std::cout << cars.ModelName << std::endl;
+   std::cout << cars.Ageofcar << std::endl;
+   std::cout << cars.RentalPrice << "$ per hour"<< std::endl;
+   if(cars.IsRented == false ) 
+   {std::cout << "Available for rent!\n";} 
+   else{std::cout << "UNAVAIBALE\n";}
 }
