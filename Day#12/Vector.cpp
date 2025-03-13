b817b0d6a0a773c7311fa25db1b6a901fca9a803
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-13 19:32:03 +0000

    push back usage
    using pushback

--- Day#12/Vector.cpp
+++ Day#12/Vector.cpp
@@ -10,4 +10,15 @@ int main()
         std::cout << num << " ";
     }
     std::cout << "\n";
+
+    numbers.push_back(123);
+    numbers.push_back(232);
+    numbers.push_back(223); // pushes numbers to the front of the vector; This is really big and i need to use this for dynamic arrays
+    
+    std::cout << "\nWith pushes: \n";
+    for(int num : numbers)
+    {
+        std::cout << num << " ";
+    }
+    std::cout << "\n";
 }
