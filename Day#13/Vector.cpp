883754497c103a0d779e08896dff20e0df1dd0b6
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-14 23:08:59 +0000

    Prime num vector array?

--- Day#13/Vector.cpp
+++ Day#13/Vector.cpp
@@ -4,6 +4,9 @@
 int main()
 {
     std::vector<int> numbers{}; // creating a empty vector
+    std::vector<int> PrimeNums{};
+
+    int PrimeNumList;
 
     numbers.push_back(1);
 
@@ -11,4 +14,9 @@ int main()
     {
         std::cout << num;
     }
+
+    // I want to make a program what checks if a number is a prime and if it is its stored in a vector array
+
+
+
 }
