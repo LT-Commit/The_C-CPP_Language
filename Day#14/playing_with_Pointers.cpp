2da889e7c09fa5c802e6e37e90c767bb3f329b48
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-15 17:11:33 +0000

    Notes
    Notes

--- Day#14/playing_with_Pointers.cpp
+++ Day#14/playing_with_Pointers.cpp
@@ -5,13 +5,13 @@ void Log(int Message);
 
 int main()
 {
-    int Num1 = 1;
-    int* pNum1 = &Num1;
-    int NewValue = 100;
-    Log(Num1);
+    int Num1 = 1; // Declares a variable called num1 and gives value of 1
+    int* pNum1 = &Num1; // Declares a intiger POINTER and names it pNum and gives the value of ADDRESS of num1
+    int NewValue = 100; // Declares a new variable of iniger for a new value
+    Log(Num1); // Call the function Log and pass the data variable of Num1 which will be 1
 
-    *pNum1 = NewValue; 
-    Log(Num1);
+    *pNum1 = NewValue;  // DEREFERENCE pNum1 Which will be the address of Num1 which we gave it and set it to the new value
+    Log(Num1); // print the new output of num 1 Which will be 100
 }
 
 void Log(int Message)
