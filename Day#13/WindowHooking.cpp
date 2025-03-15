7eb9f91766b558ec38df782e986a600d8a03c883
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-15 19:23:07 +0000

    Notes to window hookingcpp

--- Day#13/WindowHooking.cpp
+++ Day#13/WindowHooking.cpp
@@ -3,10 +3,10 @@
 
 int main()
 {
-    const std::string WindowName = "Counter-Strike 2";
+    const std::string WindowName = "Counter-Strike 2"; // Defining the window name this time as counterstrike
     RECT rec;
 
-    HWND Windowmem = FindWindow(nullptr, WindowName.c_str()); 
+    HWND Windowmem = FindWindow(nullptr, WindowName.c_str());  // Set Window mem as the find window address which return a HWND from no class and Windowname
 
     if(Windowmem) //  if hwnd is found
     {
@@ -16,13 +16,13 @@ int main()
         std::cout << "\nCSGO Not found\n";
     }
 
-    if(GetWindowRect(Windowmem,&rec))
+    if(GetWindowRect(Windowmem,&rec)) // If window memory is try along with a argu,ment with an adress valid of RECT
     {
-        std::cout << "Window position = L" << rec.left<< " T" << rec.top << "\n";
+        std::cout << "Window position = L" << rec.left<< " T" << rec.top << "\n"; // Print the position of the window left and top
     }
 
 
-    std::cout <<"Window address is: " << Windowmem;
+    std::cout <<"Window address is: " << Windowmem; // Print window address to terminal 
     SetForegroundWindow(Windowmem); // Set window to front (Forground app)
 
     // This outputs if the window was found and the HWND
