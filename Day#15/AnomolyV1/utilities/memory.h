f1557a594c2d0d10ab4f1789773da66c36088fa7
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-16 03:18:39 +0000

    Process sentry

--- Day#15/AnomolyV1/utilities/memory.h
+++ Day#15/AnomolyV1/utilities/memory.h
@@ -3,12 +3,21 @@
 #include <windows.h>
 
 #include <cstdint>
+#include <string_view>
 
 class Memory
 {
 private:
 	std::uintptr_t processId = NULL; // untptr_t is a DWORD what supports both 32 and 64 bit games and since we wanna make it universal we do it like that
 	void* processHandel = nullptr; // We want to store a handel which is also known as a void* so we make a variable for it to be stored in and set it as nullptr aka 0
+
+public:
+	// constuctions and functions go here
+	Memory(const std::string_view processName) noexcept // NO EXEPTIONS WILL optimise the code and make it faster, we are also passing in a const string view called processName
+	{
+		::PROCESSENTRY32 entry = { }; // used to store data about a process while enumirating
+		entry.dwSize = sizeof(::PROCESSENTRY32); // WINAPI what says set entry.dwSize as the size of processsentry32, without this u might expirence crashes/bugs
+	}
 };
 
 // create snapshot of all windows
