ef5b1038a946ba27ff015d137d572c4bfdf22d22
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-16 04:28:10 +0000

    Little Logo working
    Logo added for the script

--- Day#15/AnomolyV1/main.cpp
+++ Day#15/AnomolyV1/main.cpp
@@ -1,5 +1,32 @@
 #include "utilities\memory.h"
 #include "utilities\Offsets.h"
+#include <iostream>
+#include <vector>
+
+
+// MENU COLORS
+
+const std::string red = "\033[31m";
+const std::string green = "\033[32m";
+const std::string yellow = "\033[33m";
+const std::string blue = "\033[34m";
+const std::string magenta = "\033[35m";
+const std::string cyan = "\033[36m";
+const std::string white = "\033[37m";
+const std::string reset = "\033[0m";
+
+// LOGO
+std::vector<std::string> logo
+{
+	"  ___                              _       ",
+	" / _ \\                            | |      ",
+	"/ /_\\ \\_ __   ___  _ __ ___   ___ | |_   _ ",
+	"|  _  | '_ \\ / _ \\| '_ ` _ \\ / _ \\| | | | |",
+	"| | | | | | | (_) | | | | | | (_) | | |_| |",
+	"\\_| |_/_| |_|\\___/|_| |_| |_|\\___/|_|\\__, |",
+	"                                      __/ |",
+	"                                     |___/ "
+};
 
 // I want to modify health , ammo, armor, grenades 
 
@@ -15,13 +42,19 @@ int main()
 	const auto ArmorAddress = LocalPlayerAddress + Armor;
 	const auto ARammoAddress = LocalPlayerAddress + ARammo;
 
-	while (true)
-	{
+
 		memory.Write<int>(HealthAddress, 9999); //TYpecaset to int because its a 4 bytes of health
 		memory.Write<int>(ArmorAddress, 9999);
 		memory.Write<int>(ARammoAddress, 9999);
 		
 		// AYY all this works 
+	
+
+	std::cout << std::endl;
+	
+	for (const auto& line : logo)
+	{
+		std::cout << line << std::endl;
 	}
 
 
