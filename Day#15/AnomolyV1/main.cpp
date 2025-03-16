649131e0848c015f50b68131ba9f5e22615b3da1
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-16 04:07:11 +0000

    Adding a header file for offsets
    can add them later

--- Day#15/AnomolyV1/main.cpp
+++ Day#15/AnomolyV1/main.cpp
@@ -1,6 +1,12 @@
+#include "utilities\memory.h"
+#include "utilities\Offsets.h"
 
+// I want to modify health , ammo, armor, grenades 
 
 int main()
 {
+	Memory memory{ "ac_client.exe"} ; // Using the mem constructor for a snapshot of the process on our computer if we have the process open  and takes the proceess ID jsut doing it yk
+
+
 
 }
---
+++ Day#15/AnomolyV1/utilities/Offsets.h
@@ -0,0 +1,21 @@
+#pragma once
+
+//offsets will all go here
+
+// 1.2.0.2 offsets will go here
+
+//			Structures
+
+constexpr auto localPlayer = 0x10F4F4;
+constexpr auto entityList = 0x10F4F8;
+
+//			HP
+constexpr auto Health = 0xF8;
+constexpr auto Armor = 0xFC;
+
+
+//			Guns
+
+constexpr auto ARammo = 0x150;
+constexpr auto ARTimer = 0x178;
+
