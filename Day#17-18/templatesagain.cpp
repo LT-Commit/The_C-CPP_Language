3fc533ccb70e10f0b47787d3e2ed4b20ae1ee024
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-18 21:11:29 +0000

    Setting up folder

---
+++ Day#17-18/templatesagain.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+template <typename T>
+T class Numberz
+{
+private:
+
+    
+public:
+    T add(T x, T y)
+    {
+        x = x + y;
+        return x;
+    }
+    
+};
+
+
+
+int main()
+{
+    Numberz numBERs;
+}
+
