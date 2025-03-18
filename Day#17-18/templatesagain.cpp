ece905b698c282d28739baca79378fd981796ea2
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-18 21:38:25 +0000

    Explaining templates better

--- Day#17-18/templatesagain.cpp
+++ Day#17-18/templatesagain.cpp
@@ -1,24 +1,35 @@
 #include <iostream>
+#include <string>
 
-template <typename T>
-T class Numberz
+// Kind of like a macro? Not like generic at all
+
+// Templates are very powerfull which is a better way to overflow
+
+// Template is compiled by your usage/ rules
+
+// THE BEST RULE IS TO NOT COPY CODE AVOID IT and templates are the best way to do it
+
+// Best way to explain it is to show it lets say i want to print or log a value i would need to overflow but with templates its as 
+// simple as:
+
+template<typename T> // saying we will make a template -> setting template name = T
+void Log(T message) // void Log taking in an argument of message with the type T think of it like a variable for a type
 {
-private:
-
-    
-public:
-    T add(T x, T y)
-    {
-        x = x + y;
-        return x;
-    }
-    
-};
-
+    std::cout << message << std::endl;
+}
 
 
 int main()
 {
-    Numberz numBERs;
+    Log(5);
+    Log(3.3);
+    Log("Hello");
+
+    return 0;
 }
+
+
+
+
+
 
