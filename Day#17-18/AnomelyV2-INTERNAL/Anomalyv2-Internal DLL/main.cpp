2225809c486dac20cc6c4a153c94c36e27a30c59
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-18 23:29:22 +0000

    SettingDLLMain
    will finish this tommrow

--- Day#17-18/AnomelyV2-INTERNAL/Anomalyv2-Internal DLL/main.cpp
+++ Day#17-18/AnomelyV2-INTERNAL/Anomalyv2-Internal DLL/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <Windows.h>
 
 int main()
 {
+    
 
 }
+
+
+BOOL WINAPI DllMain(
+    _In_ HINSTANCE hinstDLL,
+    _In_ DWORD     fdwReason,
+    _In_ LPVOID    lpvReserved
+);
