86f97a2cdb3f56c1c929a2e46fc58c56eff8d865
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-18 21:45:12 +0000

    Type specifying with template

--- Day#17-18/templatesagain.cpp
+++ Day#17-18/templatesagain.cpp
@@ -12,7 +12,7 @@
 // Best way to explain it is to show it lets say i want to print or log a value i would need to overflow but with templates its as 
 // simple as:
 
-template<typename T> // saying we will make a template -> setting template name = T
+template<typename T> // saying we will make a template -> setting template name = T  (TEMPLATE PERAMITER)
 void Log(T message) // void Log taking in an argument of message with the type T think of it like a variable for a type
 {
     std::cout << message << std::endl;
@@ -21,10 +21,13 @@ void Log(T message) // void Log taking in an argument of message with the type T
 
 int main()
 {
-    Log(5);
-    Log(3.3);
-    Log("Hello");
+    Log<int>(5); // TYPE SPECIFIER as <int>
+    Log(5); // Dont need if its an explicit type
+    Log<float>(3.3);
+    Log<double>(3.3);
+    Log<std::string>("Hello");
 
+    std::cin.get();
     return 0;
 }
 
