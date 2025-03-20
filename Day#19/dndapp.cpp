c5d185f9932d6e8ea66beb8cc35824c34aa10470
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-20 23:45:56 +0000

    Class and notes

--- Day#19/dndapp.cpp
+++ Day#19/dndapp.cpp
@@ -14,10 +14,27 @@ Will need:
 1) main loop
 2) Player class
 3) Rand Num
+4) Create/ Delete charecter
 
 */
 
+class Player
+{
+
+public:
+int Health;
+int Mana;
+std::string name;
+    
+};
+
+
+
 int main()
 {
-    
+    while (true)
+    {
+
+
+    }
 }
