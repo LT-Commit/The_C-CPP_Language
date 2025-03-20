4bfec5fd7e6f153624a7e25bc1cc0bc1d239ff09
JamieTattersall <627719@eastridingcollege.ac.uk>
2025-03-20 23:41:00 +0000

    What i will need to setup

--- Day#19/dndapp.cpp
+++ Day#19/dndapp.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <vector>
 
+// DND APP idea will inclise
+/*
+1) ALL Dice to roll
+2) player hp - and +
+3) long rest to set hp to full
+
+*/
+
+/*
+Will need:
+1) main loop
+2) Player class
+3) Rand Num
+
+*/
+
 int main()
 {
     
